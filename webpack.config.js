45669fdbdc05de6d484511ee6b51ca10f1ebb7e6
Mateus <mateus.pegoraro@perceptvision.com.br>
2021-08-29 13:39:15 -0300

    Fast Refresh no Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // Usei o path para que isso funcione tanto no linux quanto no windows (barra invertida)
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -19,19 +20,28 @@ module.exports = {
     },
     devServer: {
         static: path.resolve(__dirname, 'public'),
+        hot: true,
     },
     plugins: [
+        isDevelopment && new ReactRefreshWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         })
-    ],
+    ].filter(Boolean), // um hack para pode usar condicionais (isDevelopment) dentro dos plugins, sem isso poderia renderizar o valor 'false' e não funcionar.
 	// 3 - Aqui definimos como trabalhar com certos tipos de arquivo
     module: {
         rules: [
             { // Definimos que para arquivos JSX vamos usar o babel para converter
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: 'babel-loader'
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                }    
             },
             { // Definimos que para arquivos CSS vamos usar o style-loader e o css-loader para converter
                 test: /\.scss$/,
